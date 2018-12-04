d8208b8fceb9470f1fdf88b9d699bb2b09512800
Mekaila quarshie <qmekaila@gmail.com>
2018-12-04 10:52:37 -0500

    Status and About page FIX!

--- ATM Machine/ATM/views.py
+++ ATM Machine/ATM/views.py
@@ -28,7 +28,7 @@ def about(request):
         "ATM/about.html",
         {
             'title': "About the ATM",
-            'content': "Exmample page for Django"
+           
         }
      )
 
@@ -90,10 +90,12 @@ def portal(request):
 
 
 def status(request):
-    ##ATM.objects.all().delete()
-    ##dummy()
+    ATM.objects.all().delete()
+    dummy()
 
     atmStat = ATM.objects.all()
     return render(request, "ATM/status.html", {'atms' : atmStat})
 
-
+def dummy():
+    atm = ATM(address = "4395 university ave", Lrefill = datetime.now(), Minbalance = 324342.66, NrefillDate = datetime.now(), balance = 2423423.66)
+    atm.save()
